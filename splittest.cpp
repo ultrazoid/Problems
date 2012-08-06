e1fb65446eadba1e11cfa9d9cd2873ff1f3f0b07
Aaron Burke <aaronbur@optusnet.com.au>
2012-08-06 21:10:17 +1000

    PROBLEM - NEED HELP C++
    So I need to have a split function. Here I have shown all the code
    required to implement my split function (aka its includes and dependant
    functions). I have included an example of its implementation in main().
    The output for the software should appear as:
    THIS
    IS
    A
    TEST
    
    Please compile & run to see what it does on your system (mine adds
    random characters to the end of each element). If you can see where I
    went wrong or how to fix it, please let me know!!
    Regards,
    Samour

---
+++ splittest.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string strRange(string superstring, int start, int end)
+{
+    if (end-start <= 0) {return string("\0");}
+    char retr[end-start];
+    for (int i=start; i<end; i++)
+    {retr[i-start] = superstring[i];}
+    return string(retr);
+}
+
+int charCount(string field, char find)
+{
+    int occs = 0;
+    for (int i=0; i<field.length(); i++)
+    {if (field[i] == find) {occs++;}}
+    return occs;
+}
+
+string* split(string original, char div)
+{
+    int len = charCount(original, div);
+    string *store = new string[len+1];
+    len = 0;
+    int pos = 0;
+    for (int i=0; i<original.length(); i++)
+    {
+        if (original[i] == div)
+        {
+            store[len] = strRange(original, pos, i);
+            len++;
+            pos = i+1;
+        }
+    }
+    store[len] = strRange(original, pos, original.length()-1);
+    return store;
+}
+
+int main()
+{
+    string test = "THIS IS A TEST";
+    for (int i=0; i<4; i++)
+    {cout << split(test, ' ')[i] << "\n";}
+    cin.get();
+}
